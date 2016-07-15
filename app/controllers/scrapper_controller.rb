91953c61457b452f896d757e98985221a5c07930
ParthivPatel-BTC <parthiv.patel@botreeconsulting.com>
2016-07-15 11:17:11 +0530

    Added more fields

--- app/controllers/scrapper_controller.rb
+++ app/controllers/scrapper_controller.rb
@@ -9,7 +9,7 @@ class ScrapperController < ApplicationController
 	def create
 		if params[:url].present?
 			begin
-				@profile = Linkedin::Profile.new(params[:url])
+				@profile = Linkedin::Profile.new(params[:url], { company_details: true, proxy_ip: '127.0.0.1', proxy_port: '3128', username: 'demo dev', password: 'demo@123' })
 			rescue Exception => e
 				puts "======>message start"
 				logger.error e.message
@@ -18,17 +18,24 @@ class ScrapperController < ApplicationController
 				redirect_to new_scrapper_path	
 			end
 			if @profile.present?
-				header = "First Name, Last Name, Designation, Organization, Linkedin Profile, City, State, Country"
+				header = "PROSPECT CODE, MINING DATE, CALLING DATE, DOMAIN, INDUSTRY SEGMENT, FUNCTIONS, FIRST NAME, LAST NAME, DESIGNATION, ORGANIZATION, EMAIL, WORK PHONE, WEBSITE, CITY, STATE, COUNTRY, PERSONAL WEBSITE"
 				dir = Dir.mkdir("#{Rails.root}/profiles") unless Dir.exists?("#{Rails.root}/profiles")
-				file = File.new("#{Rails.root}/profiles/linkedin-profiles.xlsx", "a") 
+				file = File.new("#{Rails.root}/profiles/linkedin-profiles.csv", "a") 
 				unless File.exist?(File.dirname(file))
 				  FileUtils.mkdir_p(File.dirname(file))
 				end
 				city_state_country = Nokogiri::HTML(open(params[:url])).at_css(".locality").text.split(",")
 		    File.open(file, "a:UTF-16LE:UTF-8") do |csv|
-		    	file_last_line = IO.readlines("#{Rails.root}/profiles/linkedin-profiles.xlsx", "a")
+		    	file_last_line = IO.readlines("#{Rails.root}/profiles/linkedin-profiles.csv", "a")
 		    	csv << header unless file_last_line.first.present?
 		    	csv.write "\n"
+		    	csv << ","
+		    	csv << Time.now.strftime("%d/%^b/%Y")
+		    	csv << ","
+		    	csv << ","
+		    	csv << ","
+		    	csv << ","
+		    	csv << ","
 		    	csv << @profile.first_name
 		    	csv << ","
 		    	csv << @profile.last_name
@@ -37,13 +44,16 @@ class ScrapperController < ApplicationController
 		    	csv << ","
 		    	csv << @profile.current_companies.first[:company]
 		    	csv << ","
-		    	csv << @profile.linkedin_url
 		    	csv << ","
-		    	csv << @profile.location
 		    	csv << ","
-		    	csv << city_state_country[1]
 		    	csv << ","
-		    	csv << @profile.country
+		    	csv << @profile.location rescue nil
+		    	csv << ","
+		    	csv << city_state_country[1] rescue nil
+		    	csv << ","
+		    	csv << @profile.country rescue nil
+		    	csv << ","
+		    	csv << @profile.linkedin_url rescue nil
 		    end
 		    flash[:success] = "Profile details has been added successfully"
 		  	redirect_to new_scrapper_path
@@ -52,7 +62,7 @@ class ScrapperController < ApplicationController
 	end
 
 	def download_excel
-		send_file "#{Rails.root}/profiles/linkedin-profiles.xlsx", :type => 'application/vnd.ms-excel; charset=utf-8'
+		send_file "#{Rails.root}/profiles/linkedin-profiles.csv", :type => 'application/vnd.ms-excel; charset=utf-8'
 	end
 
 	def delete_file
